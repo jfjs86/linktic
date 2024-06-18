8d4d7bdfddb7572cbed6072937950fda01b0a8cc
Juan Jimenez <jfjs86@gmail.com>
2024-06-18 12:49:13 -0500

    Front without submodule

--- 5.Frontend/ecommerce-front/src/environments/environment.ts
+++ 5.Frontend/ecommerce-front/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiProductUrl: 'http://localhost:8081/api/products',
     apiOrderUrl : 'http://localhost:8082/api/orders',
-    accessImageId : 'AKIAXTU2473GSE5J7P5D',
-    accessImagekey : 'GLtSUNWskGVxJTSixodflpf/Qu6bCeS4aLMZmhEo'
+    accessImageId : '',
+    accessImagekey : ''
   };
   
